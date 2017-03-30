95b68911018feab5c26d033e7ef9c4d329a9a621
Vladimir <nickolson000@list.ru>
2017-03-30 22:55:01 +0300

    apache telnet client

--- TelnetClientServerCalculator/src/com/test/calc/ClientLauncher.java
+++ TelnetClientServerCalculator/src/com/test/calc/ClientLauncher.java
@@ -1,9 +1,14 @@
 package com.test.calc;
 
+import javax.swing.JFrame;
+
 public class ClientLauncher {
 
 	public static void main(String[] args) {
-		System.out.println("asd");
+		JFrame frame = new JFrame("Telnet Client");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 400);
+		frame.setVisible(true);
 	}
 
 }
